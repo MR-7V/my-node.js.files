fdb0e182108c2f52356aab03a083bf5c0063a642
mohith-venkata <mohithreddy.venkata@gmail.com>
2022-09-21 00:08:29 +0530

    second commit

--- serve-html/main.js
+++ serve-html/main.js
@@ -2,9 +2,9 @@ const port = 3000,
 http = require("http")
 httpStatus = require("http-status-codes")
 fs = require("fs")
-const routeMap = {
-    "/": "views/index.html"
-}
+// const routeMap = {
+//     "/": "views/index.html"
+// }
 const getViewUrl = (url) => {
     return `views${url}.html`;
 }
